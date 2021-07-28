a93c2432faa28dff2c2f42e56bb383229f27df2d
hanranti <hanranti@outlook.com>
2021-07-28 22:14:26 +0300

    Make setting up project more straightforward
    This commit adds an example .env that can be copied as .env by only
    changing POSTGRES_PASSWORD. Vaccine-exercise-2021 should run by only
    running npm start at the projects root when docker, docker-compose and
    nodejs have been installed.

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -5,7 +5,7 @@ const PORT = process.env.BACKEND_PORT || 2222
 const db_host = process.env.DB_HOST
 const db_name = process.env.DB_NAME
 const db_user = process.env.DB_USERNAME
-const db_password = process.env.DB_PASSWORD
+const db_password = process.env.POSTGRES_PASSWORD
 
 const dialect = 'postgres'
 
--- scripts/installAndBuild.sh
+++ scripts/installAndBuild.sh
@@ -1,4 +1,6 @@
-cd frontend
+cd scripts/populate
+npm install
+cd ../../frontend
 npm install
 cd ../backend
 npm install
