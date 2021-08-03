decf28d62c6769bafc2c32d0afbf2ef9b96d07f8
hanranti <hanranti@outlook.com>
2021-08-03 13:42:46 +0300

    Test sleep time

--- scripts/buildAndPopulate.sh
+++ scripts/buildAndPopulate.sh
@@ -1,7 +1,7 @@
 ./scripts/installAndBuild.sh
 docker-compose down
 docker-compose up -d vaccine-exercise-db
-sleep 10
+sleep 30
 
 docker exec -u postgres vaccine-exercise-db psql -c "DROP DATABASE \"vaccine_db\""
 docker exec -u postgres vaccine-exercise-db psql -c "CREATE DATABASE \"vaccine_db\""
