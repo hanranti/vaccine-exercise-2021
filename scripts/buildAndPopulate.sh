c6784cfcff534e28f7ea2f9da60036714bea977a
hanranti <hanranti@outlook.com>
2021-08-03 14:26:33 +0300

    Add comments to sh files

--- scripts/buildAndPopulate.sh
+++ scripts/buildAndPopulate.sh
@@ -1,8 +1,10 @@
 ./scripts/installAndBuild.sh
+echo 'run db container'
 docker-compose down
 docker-compose up -d vaccine-exercise-db
-sleep 30
+sleep 10
 
+echo 'initialize db'
 docker exec -u postgres vaccine-exercise-db psql -c "DROP DATABASE \"vaccine_db\""
 docker exec -u postgres vaccine-exercise-db psql -c "CREATE DATABASE \"vaccine_db\""
 ./scripts/convertDataToJson.sh
--- scripts/installAndBuild.sh
+++ scripts/installAndBuild.sh
@@ -1,7 +1,11 @@
+echo 'npm installations'
 cd scripts/populate
 npm install
 cd ../../frontend
 npm install
 cd ../backend
 npm install
+cd ..
+npm install
+echo 'build docker-compose'
 docker-compose build
