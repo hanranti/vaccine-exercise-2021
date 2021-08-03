4682a5891510906b41e3123f88405e2bb7d8872d
hanranti <hanranti@outlook.com>
2021-08-03 12:47:34 +0300

    Add backend functionality for get orders
    This adds a route to get orders from any vaccine and tests for the
    controller.

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -3,6 +3,24 @@ const findAllVaccinations = async () => await db.vaccinations.findAll({
   attributes: ['vaccination-id', 'sourceBottle', 'gender', 'vaccinationDate', 'createdAt', 'updatedAt']
 })
 
+const findAllOrders = async (vaccine) => {
+  orders = []
+  vaccine.includes('Antiqua') ? orders = orders.concat(await db.Antiqua.findAll({
+    attributes: ['id', 'orderNumber', 'responsiblePerson', 'healthCareDistrict', 'vaccine', 'injections', 'arrived']
+  }))
+    : orders = orders
+  vaccine.includes('SolarBuddhica') ? orders = orders.concat(await db.SolarBuddhica.findAll({
+    attributes: ['id', 'orderNumber', 'responsiblePerson', 'healthCareDistrict', 'vaccine', 'injections', 'arrived']
+  }))
+    : orders = orders
+  vaccine.includes('Zerpfy') ? orders = orders.concat(await db.Zerpfy.findAll({
+    attributes: ['id', 'orderNumber', 'responsiblePerson', 'healthCareDistrict', 'vaccine', 'injections', 'arrived']
+  }))
+    : orders = orders
+  return orders
+}
+
 module.exports = {
-  findAllVaccinations
+  findAllVaccinations,
+  findAllOrders
 }
