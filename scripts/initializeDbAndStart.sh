23d679e909c678f5bc486cc92e56887d4347024f
hanranti <hanranti@outlook.com>
2021-08-04 14:09:32 +0300

    Test run

--- scripts/initializeDbAndStart.sh
+++ scripts/initializeDbAndStart.sh
@@ -2,10 +2,10 @@
 ./scripts/convertDataToJson.sh
 
 docker-compose down
-docker-compose up -d vaccine-exercise-db
+docker-compose run -d -T vaccine-exercise-db
 sleep 10
 docker-compose exec -T -u postgres vaccine-exercise-db psql -c "DROP DATABASE \"vaccine_db\""
 docker-compose exec -T -u postgres vaccine-exercise-db psql -c "CREATE DATABASE \"vaccine_db\""
-docker-compose up -d populate
+docker-compose run -d -T populate
 sleep 60
 docker-compose up -d --scale populate=0
